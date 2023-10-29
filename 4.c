3d4dd0fd7ab13ed4e74a044052930c0ca2070c76
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2023-10-29 18:23:24 -0300

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,30 +1,45 @@
+// Função 4
+
 void limitaSinal(double *dados, int n_amostras, int n_passos)
 {
-    // dados = [0.5, 1, 2, 3, 2, 1, 0.5];
-  int posicoes, laterais, ganho, apoio = 0;
-  double coeficiente = 0;
+    int posicoes, laterais;
+    double coeficiente = 0.0, apoio = 0.0;
 
     for (posicoes = 0; posicoes < n_amostras; posicoes++)
     { // Percorre todas as posições
 
-        if ((dados[posicoes] < -1) || (dados[posicoes] > 1))
+        if ((dados[posicoes] < -1.0) || (dados[posicoes] > 1.0))
         { // Encontrou Saturação
 
-            for(laterais = 0; laterais < n_passos; laterais++){
+            // Fórmula para determinar o coeficiente que 
+            // reduz a 1 ou -1 a saturação
+            coeficiente = 1.0 / dados[posicoes];
 
-              // 1 posicoes = x
+            // Se coeficiente for negativo, torna-o positivo
 
-              coeficiente = (1/posicoes); // 0.5
+            if (coeficiente < 0)
+                coeficiente = -coeficiente;
+
+            // Atualiza a posição
+
+            dados[posicoes] *= coeficiente;
+
+          // Atualiza o apoio que será usado para cada lateral
+          // em função de sua magnitude
+           apoio = (1 - coeficiente) / (n_passos + 1);
+
+            // Percorre n_passos laterais, alterando cada
+            for (laterais = 1; laterais <= n_passos; laterais++)
+            {
+
+               coeficiente += apoio;
               
-              dados[posicoes + laterais] = coeficiente - (0.1 + apoio) * dados[posicoes + laterais];
-              dados[posicoes - laterais] = coeficiente - (0.1 + apoio) * dados[posicoes + laterais];
-              
-            }
-                for (laterais = 1; laterais < n_passos; ){
+                // À esquerda
+                dados[(posicoes - laterais)] *= coeficiente;
 
-                    dados[i + j] *= ganho;
-                    dados[i - j] *= ganho;
-                }
+                // À direita
+                dados[(posicoes + laterais)] *= coeficiente;
+
             }
         }
     }
