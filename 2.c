704be45953761b4ac758200427676f97d6eb4688
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2023-10-29 18:22:35 -0300

    Update 2.c

--- 2.c
+++ 2.c
@@ -1,13 +1,19 @@
+// Função 2
+
 int contaSaturacoes(double* dados, int n_amostras)
 {
     int n_sat = 0, pos;
-
+  
+    // Analisa se as amostras saturam
     for (pos = 0; pos < n_amostras; pos++)
     {
-        if ((dados[pos] < 1) && (dados[pos] > -1))
+      // Se saturar (valor maior que 1 ou menor que -1, pois será módulo)
+        if (dados[pos] > 1 || dados[pos] < -1)
         {
             n_sat++;
         }
     }
+  
+    // Retorna numero de saturacoes
     return n_sat;
 }
