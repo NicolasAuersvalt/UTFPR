0ffcbd520c210bd54edd78917ff3ca76214f253d
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2023-10-29 18:22:20 -0300

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,32 +1,16 @@
-void mudaGanho(double* dados, int n_amostras, double ganho)
+// Função 1
+
+void mudaGanho(double *dados, int n_amostras, double ganho)
 {
     double apoio;
-    for (int i = 0; i < n_amostras; i++)
-    {
-        if ((ganho >= 1) || (ganho < 0))
-        {
-            apoio = dados[i]; //perguntar se eh necessario manter a variável de apoio
-            dados[i] = apoio * ganho;
-        }
-        else
-        {
-            dados[i] = 0;
-        }
-    }
-}
-
-
-// Alterado
-
-
-void mudaGanho(double* dados, int n_amostras, double ganho) 
-{   
-    int i;
 
     // Analisa as amostras e aplica o ganho
-    for (i = 0; i < n_amostras; i++) 
+    for (int i = 0; i < n_amostras; i++)
     {
-        dados[i] *= ganho;
+        if ((ganho != 1))
+        {
+            apoio = dados[i];
+            dados[i] = apoio * ganho;
+        }
     }
 }
-
