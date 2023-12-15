056505667a14e11418fd4f69cd5ea21acceb1bc3
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2023-12-15 01:41:05 -0300

    Update Arboretum.ino

--- Lab_Eletrônica/Arboretum/Files/Arboretum.ino
+++ Lab_Eletrônica/Arboretum/Files/Arboretum.ino
@@ -1,14 +1,14 @@
 /*
   ==============================================
-  
+
   樹木園 
-  
+
   Desenvolvido por:
-  
+
   Nícolas Auersvalt Marques
   Isabela Bella Bortoleto
   Vittor Henrique da Silva Masotti​
-  
+
   Versão atual (1.0)
   ==============================================
 */
@@ -38,109 +38,137 @@ SoftwareSerial EspSerial(A0, A1); // RX, TX
 
 ESP8266 wifi(&EspSerial);
 
-// Define motores nas saídas M[1] e M[2]
-AF_DCMotor motor1(1);
-AF_DCMotor motor2(2);
+// Define motores nas saídas M[3] e M[4]
+// M[3] Esquerda M[4] Direita
+
+AF_DCMotor motor1(3); // Esquerda
+AF_DCMotor motor2(4); // Direita
 
 const int max_range = 100; /* Valor máximo da velocidade */
 
-// Tempo de delay
-int time = 10;
-
-int mov(int time, int x, int y)
+int mov(int x, int y)
 {
+  
+  // Tempo de delay
+   int time = 10;
+
   int starting_position = 128;
-  int lim_nx = 108;
-  int lim_x = 148;
+
+  // Limite de deslocamento mínimo do Joystick
+  int lim_min = 90; 
+  int lim_max = 140; 
+
+  // Velocidades
+  int velocidade=0;
+  int velMin = 1;
+  int velMax = 150;
 
   /*
     Posição dos motores:
       motor1 - Esquerdo
-      motor4 - Direito
+      motor2 - Direito
   */
 
   // Acelerado
 
-  if ((x > lim_x) && (y > starting_position)) // Curva Frontal à direita
+   // Curva Frontal à direita
+
+  if ((x > lim_max) && (y > lim_max))
   {
 
-    // Inicia o Motor
-    motor1.run(BACKWARD);
-    motor1.setSpeed(y);
+    velocidade = map(y, 128, 255, velMin, velMax);
 
-    motor2.setSpeed(y);
-    motor2.run(BACKWARD);
+    motor1.setSpeed(velocidade);
+    motor1.run(FORWARD); // Esquerda
+    Serial.println();
+
+    motor2.run(RELEASE);// Direita
+    motor2.setSpeed(velocidade);
     delay(time);
   }
 
   // Curva Frontal à Esquerda
-    
-  else if ((x < lim_nx) && (y > starting_position)) 
+
+  else if ((x < lim_min) && (y > lim_max)) 
   {
 
-    motor1.setSpeed(y);
-    motor1.run(BACKWARD);
+    velocidade = map(y, 128, 255, velMin, velMax);
 
-    motor2.setSpeed(y);
-    motor2.run(FORWARD);
+    motor1.setSpeed(velocidade);
+    motor1.run(RELEASE);
+
+    motor2.setSpeed(velocidade);
+    motor2.run(BACKWARD);
     delay(time);
   }
 
   // Movimento Retrógrado
 
   // Ré para a Direita
-    
-  else if ((x > lim_x) && (y < starting_position)) 
+
+  else if ((x > lim_max) && (y < lim_min)) 
   {
 
-    motor1.setSpeed(255 - y);
-    motor1.run(FORWARD);
+    velocidade = map(y, 128, 0, velMin, velMax);
 
-    motor2.setSpeed(255 - y);
-    motor2.run(BACKWARD);
+    motor1.setSpeed(velocidade);
+    motor1.run(BACKWARD);
+
+    motor2.setSpeed(velocidade);
+    motor2.run(RELEASE);
     delay(time);
   }
 
   // Ré para a esquerda
-    
-  else if ((x < lim_nx) && (y < starting_position)) 
-  {
-    motor1.run(FORWARD);
-    motor1.setSpeed(255 - y);
 
-    motor2.setSpeed(255 - y);
-    motor2.run(BACKWARD);
+  else if ((x < lim_min) && (y < lim_min)) 
+  {
+
+    velocidade = map(y, 128, 0, velMin, velMax);
+
+    motor1.run(RELEASE);
+    motor1.setSpeed(velocidade);
+
+    motor2.setSpeed(velocidade);
+    motor2.run(FORWARD);
     delay(time);
   }
 
   // Movimento frontal
-    
-  else if ((lim_nx < x < lim_x) && (y > starting_position)) 
-  {
-    motor1.setSpeed(y);
-    motor1.run(BACKWARD);
 
-    motor2.setSpeed(y);
-    motor2.run(FORWARD);
+  else if ((lim_min < x < lim_max) && (y > lim_max)) 
+  {
+
+    velocidade = map(y, 128, 255, velMin, velMax);
+
+    motor1.setSpeed(velocidade);
+    motor1.run(FORWARD);
+
+    motor2.setSpeed(velocidade);
+    motor2.run(BACKWARD);
     delay(time);
   }
 
   // Ré
 
-  else if ((lim_nx < x < lim_x) && (y < starting_position)) 
+  else if ((lim_min < x < lim_max) && (y < lim_min)) 
   {
-    motor1.setSpeed(255 - y);
-    motor1.run(FORWARD);
 
-    motor2.setSpeed(255 - y);
-    motor2.run(BACKWARD);
+    velocidade = map(y, 128, 0, velMin, velMax);
+
+    motor1.setSpeed(velocidade);
+    motor1.run(BACKWARD);
+
+    motor2.setSpeed(velocidade);
+    motor2.run(FORWARD);
     delay(time);
   }
 
   // Parado
-    
+
   else
   {
+    
     motor1.setSpeed(y);
     motor1.run(RELEASE);
 
@@ -173,5 +201,5 @@ BLYNK_WRITE(V0)
   int y = param[1].asInt(); // Eixo Y
 
   // Chama a função de movimento
-  mov(time, x, y);
+  mov(x, y);
 }
