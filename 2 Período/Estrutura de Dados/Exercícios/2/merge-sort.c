8418d2000606395878853b792c1bee5b9add0069
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2024-03-18 18:26:20 +0000

    aslkb fka

---
+++ 2 Período/Estrutura de Dados/Exercícios/2/merge-sort.c
@@ -0,0 +1,61 @@
+#include<queue.c>
+
+Queue* merge(Queue *a, Queue *b){
+
+	// Cria uma queue do tamanho do somatório das duas
+	
+	int higher, i, val1, val2;
+	
+	Queue *queue;
+	queue = create((a->size + b->size));
+	
+	// pega o tamanho da maior delas
+	//
+	if(a->size>b->size)
+		higher = a;
+	else
+		higher = b;
+
+	// loop
+	
+	for(i=0; i<higher; i++){
+
+		val2 = dequeue(b);
+
+		if(empty(a)){
+			// Preenche com os elementos da restante
+			enqueue(queue, val2);
+		}
+		else{
+		
+			val1 = dequeue(a);
+
+			if(a>b){ // A maior q B
+				enqueue(queue, val2);
+				enqueue(queue, val1);
+			}
+
+			else{
+				enqueue(queue, val1);
+				enqueue(queue, val2);
+			}
+		}
+	}
+
+	print(queue);
+
+	return 0;
+	
+		// Verifica se a menor está vazia/cheia
+
+		
+
+		// Pega os elementos pelo Front
+
+		// compara entre eles
+		
+
+		// adiciona o maior deles e depois o menor na nova queue
+
+}
+
---
+++ 2 Período/Estrutura de Dados/Exercícios/3/parser.c
@@ -0,0 +1,48 @@
+#include "stack.h"
+
+int parser (char *c) {
+  Stack *s = create(strlen(c));
+  int i = 0;
+
+  while (c[i] != '\0') {
+    /*Terminar*/
+
+   if((c[i]=='(') || (c[i]=='[')){
+   	push(s, c[i]);
+   }
+   else if(c[i]==')'){
+   	if(empty(s) || (pop(s) == '(')){
+	       return 0;	
+	}
+	
+   }
+   else if(c[i]==']'){
+	if(empty(s) || (pop(s) == '[')){
+		return 0;
+	}
+   }
+    /*Utilizar somente as operações push, pop, empty, ... (interface)*/	  
+    i++;    
+  }
+
+  if(empty(s)){
+  	return 1;
+  }
+  else{
+  	return 0;
+  }
+
+}
+
+int main () {
+  //char *c = "( ( ) [ ( ) ] )";
+  //char *c = "( [ ) ]";
+  //char *c = "( ) [ ]";
+  //char *c = "( [ ]";
+  char *c = ") (";
+  if (parser(c)) 
+    printf("true  (bem formada)\n");
+  else  
+    printf("false (mal formada)\n");
+  return 0;
+}
