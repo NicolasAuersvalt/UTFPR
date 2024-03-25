34eb6bc23c1d66445fcbe08e1104bb4803951ead
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2024-03-25 17:33:30 +0000

    aaa

---
+++ 2 Período/Estrutura de Dados/Exercícios/4/sum.c
@@ -0,0 +1,34 @@
+#include "list.h"
+
+/* */
+int sum (List *l) {
+  /*Terminar!*/	
+
+   int sum=0;
+   
+   List *t;
+
+   for(t=l; t != NULL; t= t->next){ // o(n+1)
+	   
+     sum += t->data;
+
+  }
+
+   return sum;
+
+}
+
+
+/* */
+int main () {
+  int k;
+  List *l = NULL;
+  for (k = 0; k <= 4; k++) 
+    l = insert (l, k);
+  for (k = 9; k >= 5; k--) 
+    l = insert (l, k);
+  print (l);
+  printf("Sum = %d\n", sum(l));
+  destroy (l);
+  return 0;
+}
