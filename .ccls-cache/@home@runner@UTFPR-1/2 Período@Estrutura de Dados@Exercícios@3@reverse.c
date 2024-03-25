106df80b8efa7ee8e8cece1ac7c48a87f58575ef
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2024-03-25 03:43:27 +0000

    aq

--- .ccls-cache/@home@runner@UTFPR-1/2 Período@Estrutura de Dados@Exercícios@3@reverse.c
+++ .ccls-cache/@home@runner@UTFPR-1/2 Período@Estrutura de Dados@Exercícios@3@reverse.c
@@ -1,12 +1,39 @@
-#include "stack.h"
+// #include "stack.h"
 #include "queue.h"
 
 /* */
 void reverse (Queue *q) {
-  /*Terminar*/	
+ /*Terminar*/	
+	Queue *t, *aux;
+	int i, size = getsize_queue(q);
+  
+	t = create_queue(size);
+
+	for(i=1; i<size; i++){
+
+		int apoio = dequeue(q);	
+		enqueue(t, apoio);
+
+	}
+
+  int apoiao = front(t);
+
+  int apoio = t->front;
+  t->front = (t->back)-1;
+  t->back = apoio;
+
+  
+
+  for(i=1; i<size-1; i++){
+    
+    int apoio = front(t);	
+    enqueue(q, apoio);
+    (t->front)--;
+  
+  }
+  enqueue(q, apoiao);
 }
 
-/* */
 int main () {
   int i, n = 10;
   Queue *q = create_queue (n+1);
