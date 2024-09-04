058fd473d25bf87f8fc6594f627550f86fd8203e
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2024-09-04 23:16:29 +0000

    SA

---
+++ 2 Periodo/Calculo 2/11.5/4.py
@@ -0,0 +1,17 @@
+from sympy import Symbol, oo, sqrt, Limit
+
+# Definindo a variável simbólica
+n = Symbol('n')
+
+# Definindo o termo da série alternada
+series_term = (-1)**n / sqrt(n)
+
+# Verificando se os termos convergem para zero
+limit_expression = Limit(1 / sqrt(n), n, oo).doit()
+
+# Verificando o limite
+if limit_expression == 0:
+    # A série converge pelo teste da série alternada
+    print("A série converge pelo Teste da Série Alternada.")
+else:
+    print("A série diverge.")
