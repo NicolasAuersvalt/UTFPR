c00e62e8c4f8693e3acc55ca3bad458c4e1a245c
Nícolas Auersvalt <nicolasauersvalt@gmail.com>
2024-09-04 23:58:19 +0000

    asda

--- 2 Periodo/Estatística/IrisData/Desenvolvimento/header.py
+++ 2 Periodo/Estatística/IrisData/Desenvolvimento/header.py
@@ -1,14 +1,16 @@
 import streamlit as st
-import seaborn as sns
-import matplotlib.pyplot as plt
-import pandas as pd
-from sklearn.datasets import load_iris
 import numpy as np
+import matplotlib.pyplot as plt
 from scipy.stats import norm
-from sklearn.cluster import KMeans
-from scipy.spatial.distance import pdist
-from scipy.cluster.hierarchy import linkage, dendrogram, fcluster
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.model_selection import cross_val_score
+from scipy.spatial.distance import pdist
+from scipy.cluster.hierarchy import linkage, fcluster, dendrogram
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+import seaborn as sns
+import pandas as pd
