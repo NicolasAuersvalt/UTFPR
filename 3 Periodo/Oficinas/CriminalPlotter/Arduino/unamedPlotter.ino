4d0a76afb3d076472d0b5a6dac601d5dc3fac609
Nícolas <nicolasauersvalt@gmail.com>
2025-01-04 05:49:01 -0300

    cp

--- 3 Periodo/Oficinas/CriminalPlotter/Arduino/unamedPlotter.ino
+++ 3 Periodo/Oficinas/CriminalPlotter/Arduino/unamedPlotter.ino
@@ -1,110 +1,102 @@
-/*
-Federal University of Technology - Paraná (UTFPR)
-by Nícolas Auersvalt, 2024.
-*/
+const int pot = A0;
+int i;
+int motorX[] = {2,3,4,5};
+int motorY[] = {6,7,8,9};
+int motorZ[] = {10,11,12,13};
+int horario = 0;
 
-// Definições de pinos para os motores
-#define MOTOR_X_PWM 3      // Pino PWM para velocidade do motor X
-#define MOTOR_X_INPUT1 4   // Pino para controle de direção (IN1 do L293D) do motor X
-#define MOTOR_X_INPUT2 5   // Pino para controle de direção (IN2 do L293D) do motor X
-
-#define MOTOR_Y_PWM 8      // Pino PWM para velocidade do motor Y
-#define MOTOR_Y_INPUT1 7   // Pino para controle de direção (IN1 do L293D) do motor Y
-#define MOTOR_Y_INPUT2 6   // Pino para controle de direção (IN2 do L293D) do motor Y
-
-#define MOTOR_INC_PWM 11    // Pino PWM para velocidade do motor de inclinação
-#define MOTOR_INC_INPUT1 9  // Pino para controle de direção (IN1 do L293D) do motor de inclinação
-#define MOTOR_INC_INPUT2 10 // Pino para controle de direção (IN2 do L293D) do motor de inclinação
-
-#define POT1 A2 // Pino analógico do primeiro potenciômetro (motor X)
-#define POT2 A1 // Pino analógico do segundo potenciômetro (motor Y)
-#define POT3 A0 // Pino analógico do terceiro potenciômetro (motor de inclinação)
-
-
-// Range Potenciometro (0, 1023)
-#define limPot 512 // Centralizar o potenciômetro
+const int velocidade = 97;
+const int passo = 100;
 
 void setup() {
-  // Configurar pinos dos motores
-  pinMode(MOTOR_X_PWM, OUTPUT);
-  pinMode(MOTOR_X_INPUT1, OUTPUT);
-  pinMode(MOTOR_X_INPUT2, OUTPUT);
 
-  pinMode(MOTOR_Y_PWM, OUTPUT);
-  pinMode(MOTOR_Y_INPUT1, OUTPUT);
-  pinMode(MOTOR_Y_INPUT2, OUTPUT);
-
-  pinMode(MOTOR_INC_PWM, OUTPUT);
-  pinMode(MOTOR_INC_INPUT1, OUTPUT);
-  pinMode(MOTOR_INC_INPUT2, OUTPUT);
-
-  // Configurar pinos dos potenciômetros
-  pinMode(POT1, INPUT);
-  pinMode(POT2, INPUT);
-  pinMode(POT3, INPUT);
-
-  // Iniciar comunicação serial
+  
   Serial.begin(9600);
+
+  for( i=0 ; i < 4 ; i++ ){
+    pinMode(motorX[i],OUTPUT); // Configura os pinos como saída
+    pinMode(motorY[i],OUTPUT); // Configura os pinos como saída
+    pinMode(motorZ[i],OUTPUT); // Configura os pinos como saída
+  }
+}
+
+void mover(int giro, bool horario, int motor[]){
+
+  while(giro--){
+  
+    if(horario == true){ // Aciona o motor no sentido Horário
+      
+      for( i=0 ; i < 4 ; i++ ){  // Intercala o as bobinas acionadas
+        
+         digitalWrite(motor[i],HIGH); // Envia um pulso de um passo
+         delay(100-velocidade); 
+         digitalWrite(motor[i],LOW);
+      }
+    }
+
+    if(horario == false){ // Aciona o motor no sentido Anti-Horário
+      
+      for( i=4 ; i > -1 ; i-- ){ // Intercala o as bobinas acionadas
+        
+         digitalWrite(motor[i],HIGH); // Envia um pulso de um passo
+         delay(100-velocidade);
+         digitalWrite(motor[i],LOW); 
+      }
+    }
+    
+  }
+
+}
+
+void canetar(int *giros, bool *horario, int motorZ){
+
+    *horario = true;
+    mover(giros, horario, motorZ); // Desce
+
+    *horario = false;
+    mover(giros, horario, motorZ); // Sobe
+
 }
 
 void loop() {
-  // Ler os valores dos potenciômetros
-  int potValueX = analogRead(POT1);
-  int potValueY = analogRead(POT2);
-  int potValueInc = analogRead(POT3);
-  int motorSpeedX, motorSpeedY, motorSpeedInc;
 
-  // Controlar motor X
-  if (potValueX < limPot) {
-    motorSpeedX = map(potValueX, 0, 511, 255, 0); // Velocidade máxima até zero
-    digitalWrite(MOTOR_X_INPUT1, HIGH);
-    digitalWrite(MOTOR_X_INPUT2, LOW);
-  } else {
-    motorSpeedX = map(potValueX, 512, 1023, 0, 255); // Zero até velocidade máxima
-    digitalWrite(MOTOR_X_INPUT1, LOW);
-    digitalWrite(MOTOR_X_INPUT2, HIGH);
+    int matriz[10][10] = {
+    {0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+    {1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+    {0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+    {1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+    {0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+    {1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+    {0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+    {1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
+    {0, 1, 0, 1, 0, 1, 0, 1, 0, 1},
+    {1, 0, 1, 0, 1, 0, 1, 0, 1, 0}
+  };
+
+  bool horario = false;
+  int giros = passo;
+  int sizeMat = 10;
+
+  for (int i = 0; i < sizeMat; i++) {
+    
+    for (int j = 0; j < sizeMat; j++) {
+          
+      if(matriz[i][j]==1){
+
+        canetar(&giros, &horario, motorZ);
+
+      }
+    horario = true;
+    mover(giros, horario, motorX); // em x
   }
-  analogWrite(MOTOR_X_PWM, motorSpeedX);
+  horario = true;
+  mover(giros, horario, motorY); 
 
-  // Controlar motor Y
-  if (potValueY < limPot) {
-    motorSpeedY = map(potValueY, 0, 511, 255, 0); // Velocidade máxima até zero
-    digitalWrite(MOTOR_Y_INPUT1, HIGH);
-    digitalWrite(MOTOR_Y_INPUT2, LOW);
-  } else {
-    motorSpeedY = map(potValueY, 512, 1023, 0, 255); // Zero até velocidade máxima
-    digitalWrite(MOTOR_Y_INPUT1, LOW);
-    digitalWrite(MOTOR_Y_INPUT2, HIGH);
+  horario = false;
+  mover(giros * sizeMat, horario, motorX); 
   }
-  analogWrite(MOTOR_Y_PWM, motorSpeedY);
 
-  // Controlar motor de inclinação
-  if (potValueInc < limPot) {
-    motorSpeedInc = map(potValueInc, 0, 511, 255, 0); // Velocidade máxima até zero
-    digitalWrite(MOTOR_INC_INPUT1, HIGH);
-    digitalWrite(MOTOR_INC_INPUT2, LOW);
-  } else {
-    motorSpeedInc = map(potValueInc, 512, 1023, 0, 255); // Zero até velocidade máxima
-    digitalWrite(MOTOR_INC_INPUT1, LOW);
-    digitalWrite(MOTOR_INC_INPUT2, HIGH);
-  }
-  analogWrite(MOTOR_INC_PWM, motorSpeedInc);
+  horario = false;
+  mover(giros * sizeMat, horario, motorY); 
 
-  // Monitorar os valores dos potenciômetros e as velocidades dos motores
-  Serial.print("Pot1: ");
-  Serial.print(potValueX);
-  Serial.print(" | Velocidade Motor X: ");
-  Serial.println(motorSpeedX);
-
-  Serial.print("Pot2: ");
-  Serial.print(potValueY);
-  Serial.print(" | Velocidade Motor Y: ");
-  Serial.println(motorSpeedY);
-
-  Serial.print("Pot3: ");
-  Serial.print(potValueInc);
-  Serial.print(" | Velocidade Motor Inclinação: ");
-  Serial.println(motorSpeedInc);
-
-  delay(50); // Resposta mais rápida
 }
